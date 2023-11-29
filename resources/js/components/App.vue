4e78f09ecf79bed7c8659bb1f9857032eaba6e4a
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-11-29 18:28:01 +0200

    create main component with router view and link to all fruits

---
+++ resources/js/components/App.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: "App"
+}
+</script>
+
+<template>
+    <div>
+        <router-link :to="{name: 'fruit.index'}">List</router-link>
+        <router-view></router-view>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
