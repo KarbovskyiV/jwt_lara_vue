e31e8b55a6b114ec249f4bc685649a878a25b316
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-11-30 17:37:09 +0200

    add showing all fruits form db

--- resources/js/components/Fruit/Index.vue
+++ resources/js/components/Fruit/Index.vue
@@ -1,12 +1,48 @@
 <script>
+import axios from "axios";
+
 export default {
-    name: "Index"
+    name: "Index",
+
+    data() {
+        return {
+            fruits: []
+        }
+    },
+
+    mounted() {
+        this.getAll()
+    },
+
+    methods: {
+        getAll() {
+            axios.get('/api/fruits')
+                .then(res => {
+                    this.fruits = res.data.data
+                })
+        }
+    }
 }
 </script>
 
 <template>
     <div>
-        List of fruits
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Price</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="fruit in fruits">
+                <th scope="row">{{ fruit.id }}</th>
+                <td>{{ fruit.name }}</td>
+                <td>{{ fruit.price }}</td>
+            </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
