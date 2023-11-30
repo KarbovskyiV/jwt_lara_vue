14adc68070d7959f262aa050058220a9ce618881
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-11-30 17:32:53 +0200

    register 2 users route

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,9 +1,17 @@
 import {createRouter, createWebHistory} from 'vue-router';
 import Index from "./components/Fruit/Index.vue";
+import Login from "./components/User/Login.vue";
+import Registration from "./components/User/Registration.vue";
 
 const routes = [
     {
         path: '/fruits', component: Index, name: 'fruit.index'
+    },
+    {
+        path: '/users/login', component: Login, name: 'user.login'
+    },
+    {
+        path: '/users/registration', component: Registration, name: 'user.registration'
     },
 ]
 
