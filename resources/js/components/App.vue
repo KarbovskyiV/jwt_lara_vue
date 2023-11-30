1ba1d2e25aa2d67c9c93885897dd7a3febf51f15
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-11-30 17:33:47 +0200

    add 2 link to login and register

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -7,6 +7,8 @@ export default {
 <template>
     <div>
         <router-link :to="{name: 'fruit.index'}">List</router-link>
+        <router-link :to="{name: 'user.login'}" class="m-3">Login</router-link>
+        <router-link :to="{name: 'user.registration'}">Registration</router-link>
         <router-view></router-view>
     </div>
 </template>
