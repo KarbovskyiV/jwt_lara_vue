4d20adf285f17ca2aa9b7f5899e84fed0be25bc9
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-11-30 17:38:24 +0200

    create register component with form

---
+++ resources/js/components/User/Registration.vue
@@ -0,0 +1,45 @@
+<script>
+import axios from "axios";
+
+export default {
+    name: "Registration",
+
+    data() {
+        return {
+            name: '',
+            email: '',
+            password: '',
+            password_confirmation: '',
+        }
+    },
+
+    methods: {
+        store() {
+            axios.post('/api/users', {
+                name: this.name,
+                email: this.email,
+                password: this.password,
+                password_confirmation: this.password_confirmation
+            })
+                .then(res => {
+                    console.log(res);
+                })
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="w-25">
+        <input type="text" v-model="name" class="form-control mt-3 mb-3" placeholder="name">
+        <input type="email" v-model="email" class="form-control mb-3" placeholder="email">
+        <input type="password" v-model="password" class="form-control mb-3" placeholder="password">
+        <input type="password" v-model="password_confirmation" class="form-control mb-3"
+               placeholder="password confirmation">
+        <input @click.prevent="store" type="submit" class="btn btn-primary" value="Register">
+    </div>
+</template>
+
+<style scoped>
+
+</style>
